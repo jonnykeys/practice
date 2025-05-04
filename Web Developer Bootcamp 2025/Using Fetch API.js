ff3a541b3b80220063218c67cdf2612158256ca3
jonnykeys <jonnylcolbert@gmail.com>
2025-05-04 12:28:21 -0500

    updates

---
+++ Web Developer Bootcamp 2025/Using Fetch API.js
@@ -0,0 +1,31 @@
+// fetch("https://swapi.dev/api/people/1/")
+//     .then((res) => {
+//         console.log("RESOLVED!", res);
+//         return res.json();
+//     })
+//     .then((data) => {
+//         console.log(data);
+//     })
+//     .catch((e) => {
+//         console.log("ERROR!", e);
+//     });
+
+fetch("https://swapi.dev/api/people/1/")
+    .then((res) => {
+        console.log("RESOLVED!", res);
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+        return fetch("https://swapi.dev/api/people/2/");
+    })
+    .then((res) => {
+        console.log("SECOND REQUEST RESOLVED!");
+        return res.json();
+    })
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((e) => {
+        console.log("ERROR!", e);
+    }); 
