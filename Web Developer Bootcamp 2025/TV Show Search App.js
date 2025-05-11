59e7c7e07e97b63f37efb2db9724d8ed3e7d19a1
jonnykeys <jonnylcolbert@gmail.com>
2025-05-11 16:15:48 -0500

    updates

--- Web Developer Bootcamp 2025/TV Show Search App.js
+++ Web Developer Bootcamp 2025/TV Show Search App.js
@@ -1,5 +1,18 @@
-const form = document.querySelector('searchForm');
-form.addEventListener('submit', function (e) {
+const form = document.querySelector('#searchForm');
+form.addEventListener('submit', async function (e) {
     e.preventDefault();
-    console.log('SUBMITTED!')
-});
+    const searchTerm = form.elements.query.value;
+    const res = await axios.get(`http://api.tvmaze.com/search/shows?q=${searchTerm}`)
+    makeImages(res.data)
+    form.elements.query.value = '';
+})
+
+const makeImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        }
+    }
+}
