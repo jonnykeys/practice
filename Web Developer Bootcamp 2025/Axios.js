e178cc79a3cb445db59c538a1855a2343c5dddf4
jonnykeys <jonnylcolbert@gmail.com>
2025-05-10 23:37:54 -0500

    updates

--- Web Developer Bootcamp 2025/Axios.js
+++ Web Developer Bootcamp 2025/Axios.js
@@ -1,1 +1,28 @@
-axios.get("https://swapi.dev/api/people/1/");
+// axios.get("https://pokeapi.co/api/v2/pokemon/charmander")
+//     .then(res => {
+//         console.log("RESPONSE:", res);
+//     });
+// .catch (e => {
+//     console.log("ERROR:", e);
+// });
+
+// const getPokemon = async (id) => {
+//     try {
+//         const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+//         console.log(res.data);
+//     } catch (e) {
+//         console.log("ERROR:", e);
+//     }
+// };
+
+// getPokemon("charizard");
+// getPokemon(9);
+
+
+const getDadJoke = async () => {
+    const config = { headers: { Accept: 'application/json' } }
+    const res = await axios.get("https://icanhazdadjoke.com/", config)
+    console.log(res.data.joke)
+}
+
+getDadJoke();
